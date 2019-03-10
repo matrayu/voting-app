4e53325579d353a0f1ed82491de62f216041089b
matrayu <northridge614@gmail.com>
2019-03-10 11:34:56 -0700

    Completed Walkthru
    Finished walkthru of tutorial for voting app.

--- public/js/app.js
+++ public/js/app.js
@@ -4,23 +4,48 @@ class ProductList extends React.Component {
   };
 
   componentDidMount() {
-    this.setState({ products: Seed.products });
+    this.setState({ products: Seed.products })
   }
 
   handleProductUpVote = (productId) => {
+    console.log(`${productId} was upvoted.`)
+    /* traverse the products array
+    Importantly, map() returns a new array as opposed to modifying the array this.state.products. */
     const nextProducts = this.state.products.map((product) => {
+      /* check if the current product matches productId */
       if (product.id === productId) {
+        /* overwrite the votes property on the new product object and increment vote count */ 
         return Object.assign({}, product, {
           votes: product.votes + 1,
-        });
-      } else {
+        })
+      }
+      /* If the current product is not the one specified by productId, we return it unmodified: */
+      else {
         return product;
       }
-    });
+    })
     this.setState({
       products: nextProducts,
     });
   }
+
+  handleProductDownVote = (productId) => {
+    console.log(`${productId} was down voted.`)
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes - 1,
+        })
+      }
+      else {
+        return product;
+      }
+    })
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
 
   render() {
     const products = this.state.products.sort((a, b) => (
@@ -28,7 +53,7 @@ class ProductList extends React.Component {
     ));
     const productComponents = products.map((product) => (
       <Product
-        key={'product-' + product.id}
+        key={`product-${product.id}`}
         id={product.id}
         title={product.title}
         description={product.description}
@@ -36,9 +61,10 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
-        onVote={this.handleProductUpVote}
+        onUpVote={this.handleProductUpVote}
+        onDownVote={this.handleProductDownVote}
       />
-    ));
+    ))
     return (
       <div className='ui unstackable items'>
         {productComponents}
@@ -48,9 +74,14 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+
   handleUpVote = () => (
-    this.props.onVote(this.props.id)
-  );
+    this.props.onUpVote(this.props.id)
+  )
+
+  handleDownVote = () => (
+    this.props.onDownVote(this.props.id)
+  )
 
   render() {
     return (
@@ -61,17 +92,18 @@ class Product extends React.Component {
         <div className='middle aligned content'>
           <div className='header'>
             <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
+              <i className='larget caret up icon' />
             </a>
             {this.props.votes}
+            <a onClick={this.handleDownVote}>
+              <i className='larget caret down icon' />
+            </a>
           </div>
           <div className='description'>
             <a href={this.props.url}>
               {this.props.title}
             </a>
-            <p>
-              {this.props.description}
-            </p>
+            <p>{this.props.description}</p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
@@ -85,6 +117,7 @@ class Product extends React.Component {
     );
   }
 }
+
 
 ReactDOM.render(
   <ProductList />,
